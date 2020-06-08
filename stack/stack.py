795d50e18bb348b60cb66fa58944035e0d467b0c
Schrese <schreseholloway@gmail.com>
2020-06-08 18:08:58 -0400

    Finished first stack.py next is with the linked-list

--- stack/stack.py
+++ stack/stack.py
@@ -11,22 +11,45 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 class Stack:
-    def __init__(self):
+    def __init__(self, storage=[]):
         self.size = 0
-        # self.storage = ?
+        self.storage = storage
 
     def __len__(self):
-        pass
+        # pass
         # We'll use the "len()" List method 
+        # self.size = len(self.storage)
         # Return "0" for an empty array
+        if self.size == 0:
+            return 0
         # Otherwise, return the length of the array
+        else:
+            return self.size
 
     def push(self, value):
-        pass
+        # pass
         # We'll use the "append()" List method
+        new_list = self.storage.append(value)
+        self.size += 1
+        return new_list
         # We shouldn't need conditionals here since if there's nothing in the stack, it will add to the end, and if there is something in the stack, then it'll still add to the end
 
     def pop(self):
-        pass
+        # pass
         # We'll use the "pop()" List method with no parameters passed in
+        if self.size > 0:
+            # print(self.size)
+            # val = self.storage[self.size]
+            self.size -= 1
+            return self.storage.pop()
+
+            # print(val)
+
+            # return something
+        # if self.size == 0:
+        #     return 0
+        # else:
+        #     self.storage.pop()
+        #     self.size -= 1
+
         # This already returns the element that has been removed, and by default will remove the last element in the list
