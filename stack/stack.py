b9ea4f5ad48a230d0c4c3a4742e1085576c915d4
Schrese <schreseholloway@gmail.com>
2020-06-08 16:53:50 -0400

    Planned out the stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -17,9 +17,16 @@ class Stack:
 
     def __len__(self):
         pass
+        # We'll use the "len()" List method 
+        # Return "0" for an empty array
+        # Otherwise, return the length of the array
 
     def push(self, value):
         pass
+        # We'll use the "append()" List method
+        # We shouldn't need conditionals here since if there's nothing in the stack, it will add to the end, and if there is something in the stack, then it'll still add to the end
 
     def pop(self):
         pass
+        # We'll use the "pop()" List method with no parameters passed in
+        # This already returns the element that has been removed, and by default will remove the last element in the list
