61cb4d7b8c99c67a294b6adfc2681c144ffcd3c5
Schrese <schreseholloway@gmail.com>
2020-06-08 18:27:11 -0400

    Finished second part of stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -10,46 +10,74 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from singly_linked_list import Node, LinkedList
+
+# BELOW IS WITH THE SINGLY LINKED LIST
+
 class Stack:
-    def __init__(self, storage=[]):
+    def __init__(self):
         self.size = 0
-        self.storage = storage
+        self.storage = LinkedList()
 
     def __len__(self):
         # pass
-        # We'll use the "len()" List method 
-        # self.size = len(self.storage)
-        # Return "0" for an empty array
-        if self.size == 0:
-            return 0
-        # Otherwise, return the length of the array
-        else:
-            return self.size
+        return self.size
 
     def push(self, value):
         # pass
-        # We'll use the "append()" List method
-        new_list = self.storage.append(value)
         self.size += 1
-        return new_list
-        # We shouldn't need conditionals here since if there's nothing in the stack, it will add to the end, and if there is something in the stack, then it'll still add to the end
+        return self.storage.add_to_tail(value)
 
     def pop(self):
         # pass
-        # We'll use the "pop()" List method with no parameters passed in
-        if self.size > 0:
-            # print(self.size)
-            # val = self.storage[self.size]
+        if self.size == 0:
+            return None
+        else:
             self.size -= 1
-            return self.storage.pop()
+            return self.storage.remove_tail()
 
-            # print(val)
+# BELOW IS WITH PLAIN PYTHON LISTS 
 
-            # return something
-        # if self.size == 0:
-        #     return 0
-        # else:
-        #     self.storage.pop()
-        #     self.size -= 1
+# class Stack:
+#     def __init__(self, storage=[]):
+#         self.size = 0
+#         self.storage = storage
 
-        # This already returns the element that has been removed, and by default will remove the last element in the list
+#     def __len__(self):
+#         # pass
+#         # We'll use the "len()" List method 
+#         # self.size = len(self.storage)
+#         # Return "0" for an empty array
+#         if self.size == 0:
+#             return 0
+#         # Otherwise, return the length of the array
+#         else:
+#             return self.size
+
+#     def push(self, value):
+#         # pass
+#         # We'll use the "append()" List method
+#         new_list = self.storage.append(value)
+#         self.size += 1
+#         return new_list
+#         # We shouldn't need conditionals here since if there's nothing in the stack, it will add to the end, and if there is something in the stack, then it'll still add to the end
+
+#     def pop(self):
+#         # pass
+#         # We'll use the "pop()" List method with no parameters passed in
+#         if self.size > 0:
+#             # print(self.size)
+#             # val = self.storage[self.size]
+#             self.size -= 1
+#             return self.storage.pop()
+
+#             # print(val)
+
+#             # return something
+#         # if self.size == 0:
+#         #     return 0
+#         # else:
+#         #     self.storage.pop()
+#         #     self.size -= 1
+
+#         # This already returns the element that has been removed, and by default will remove the last element in the list
